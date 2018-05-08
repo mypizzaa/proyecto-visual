856fadce14ac90cc9406797e0842c4f0ec90e9e8
david <david.ramirez.sds@gmail.com>
2018-05-08 10:02:51 +0200

    Prueba

--- MyPizza/MyPizza/LocalizadorEnvios.Designer.cs
+++ MyPizza/MyPizza/LocalizadorEnvios.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -75,11 +76,21 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Numero de pedido :";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // LocalizadorEnvios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -99,5 +110,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
