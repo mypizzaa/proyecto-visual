717048e13e5caf883e5cb223599cc688f7436101
kyreskyres <38668076+kyreskyres@users.noreply.github.com>
2018-05-09 12:46:31 +0200

    Add files via upload

--- myPizza.sql
+++ myPizza.sql
@@ -3,126 +3,207 @@ CREATE USER 'administrator'@'localhost' IDENTIFIED BY 'adminpsw';
 
 CREATE DATABASE mypizza  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;
 
--- Permisos
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON mypizza.* TO 'administrator'@'localhost';
 
 USE mypizza;
 
--- TABLAS DE USUARIOS
-CREATE TABLE `tb_cliente` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `dni` VARCHAR(9) DEFAULT NULL UNIQUE,
-    `nombre` VARCHAR(40) DEFAULT NULL,
-    `apellidos` VARCHAR(40) DEFAULT NULL,
-    `password` VARCHAR(40) DEFAULT NULL,
-    `direccion1` VARCHAR(40) DEFAULT NULL,
-    `direccion2` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+
+CREATE TABLE `tb_pizza`(
+    `id_pizza` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_producto` INT(4) NOT NULL,
+    PRIMARY KEY (`id_pizza`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `tb_empleado` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `dni` VARCHAR(9) DEFAULT NULL UNIQUE,
-    `nombre` VARCHAR(40) DEFAULT NULL,
-    `apellidos` VARCHAR(40) DEFAULT NULL,
-    `password` VARCHAR(40) DEFAULT NULL,
-    `hora_entrada` VARCHAR(40) DEFAULT NULL,
-    `hora_salida` VARCHAR(40) DEFAULT NULL,
-    `horas_semanales` VARCHAR(40) DEFAULT NULL,
-    `salario` DOUBLE DEFAULT 0.0,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-CREATE TABLE `usuarios` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `nickname` VARCHAR(40) DEFAULT NULL ,
-    `password` VARCHAR(40) DEFAULT NULL ,
-    `role` VARCHAR(40) DEFAULT NULL,
-    `id_tb_empleado` 
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-
-CREATE TABLE `tb_pizza_info` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(40) DEFAULT NULL,
-    `descripcion` VARCHAR(40) DEFAULT NULL,
-    `imagen` VARCHAR(40) DEFAULT NULL,
-    `precio` DOUBLE DEFAULT 0.0,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-CREATE TABLE `tb_pizza` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `id_ingredientes` INT(4) NOT NULL,
-    `id_pizza_info` INT(4) NOT NULL,
+CREATE TABLE `tb_pizzaDetalle` (
+	`id` INT(4) NOT NULL AUTO_INCREMENT,
+	`id_ingrediente` INT(4) NOT NULL,
+    `id_pizza` INT(4) NOT NULL,    
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `tb_ingredientes` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
+	`id_ingrediente` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_producto` INT(4) NOT NULL,    
+    PRIMARY KEY (`id_ingrediente`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `tb_producto` (
+    `id_producto` INT(4) NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(40) DEFAULT NULL,
     `precio` DOUBLE DEFAULT 0.0,
-    PRIMARY KEY (`id`)
+    `imagen` VARCHAR(40),
+    `id_tipo` INT(4) NOT NULL,
+    PRIMARY KEY (`id_producto`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `tb_refresco` (
+    `id_refresco` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_producto` INT(4) NOT NULL,
+    PRIMARY KEY (`id_refresco`)
 ) ENGINE=InnoDB;
 
 
 
 
+CREATE TABLE `tb_tipo` (
+    `id_tipo` INT(4) NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(40),
+    PRIMARY KEY (`id_tipo`)
+) ENGINE=InnoDB;
 
 CREATE TABLE `tb_pedido_info` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `id_cliente` INT(4) NOT NULL,
-    `id_empleado` INT(4) NOT NULL,
-    `direccion` VARCHAR(40) DEFAULT NULL,
-    `estado` VARCHAR(40) DEFAULT NULL,
-    `fecha` DATE,
-    `hora` TIME,
-    PRIMARY KEY (`id`)
+    `id_pedido_info` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_estado` INT(4),
+    `direccion` VARCHAR(40),
+    `dia_hora` DATETIME,
+    PRIMARY KEY (`id_pedido_info`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `tb_pedido` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `id_pizza` INT(4) NOT NULL,
-    `id_ingredientes` INT(4) NOT NULL,
-    `id_pedido` DOUBLE DEFAULT 0.0,
-    PRIMARY KEY (`id`)
+	`id_pedido` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_pedido_info` INT(4),
+    `id_producto` INT(4),
+    `observaciones` VARCHAR(40),
+    `id_cliente` INT(4),
+    PRIMARY KEY (`id_pedido`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `tb_pizza_pedido` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `id_pedido_info` INT(4) NOT NULL,
+CREATE TABLE `tb_estado` (
+	`id_estado` INT(4) NOT NULL AUTO_INCREMENT,
+    `observaciones` VARCHAR(40),
+    `id_empleado` INT(4),
+    PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `tb_detalleFactura` (
+	`id_detalleFactura` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_pedido` INT(4),
+    `id_factura` INT(4),
+    `cantidad` INT(4),
     `precio_total` DOUBLE DEFAULT 0.0,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id_detalleFactura`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `tb_otros_productos_pedido` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `id_pedido` INT(4) NOT NULL,
-    `id_otros_productos` INT(4) NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE `tb_Factura` (
+	`id_factura` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_cliente` INT(4),
+    `id_metodoPago` INT(4),
+    `fecha` DATE,
+    `cobrado` BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (`id_factura`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `tb_otros_productos` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `id_categoria_producto` INT(4) NOT NULL,
+CREATE TABLE `tb_MetodoPago` (
+	`id_metodoPago` INT(4) NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(40),
+    `otros_detalles` VARCHAR(40),
+    PRIMARY KEY (`id_metodoPago`)
+) ENGINE=InnoDB;
+
+
+
+CREATE TABLE `tb_empleado` (
+    `id_empleado` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_usuario` INT(4),
+    `hora_entrada` DATETIME,
+    `hora_salida` DATETIME,
+    `horas_semanales` int(4),
+    `salario` DOUBLE DEFAULT 0.0,  
+    PRIMARY KEY (`id_empleado`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `Usuario` (
+    `id_usuario` INT(4) NOT NULL AUTO_INCREMENT,
+    `dni` VARCHAR(9) DEFAULT NULL UNIQUE,
     `nombre` VARCHAR(40) DEFAULT NULL,
-    `descripcion` VARCHAR(40) DEFAULT NULL,    
-    `precio` DOUBLE DEFAULT 0.0,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-CREATE TABLE `tb_categoria_producto` (
-    `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `apellidos` VARCHAR(40) DEFAULT NULL,
+    `contraseña` VARCHAR(40) DEFAULT NULL,
+    `imagen` VARCHAR(40) DEFAULT NULL,
+    `tipo_usuario` VARCHAR(40) DEFAULT NULL,
+    `correo` VARCHAR(40) NOT NULL UNIQUE,
+    PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB;
 
 
+CREATE TABLE `tb_cliente` (
+    `id_cliente` INT(4) NOT NULL AUTO_INCREMENT,
+    `id_usuario` INT(4),
+    `direccion1` VARCHAR(40) DEFAULT NULL,
+    `direccion2` VARCHAR(40) DEFAULT NULL,   
+    PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB;
 
 
---alter table products
---add foreign key (id_category) references category(id_category)
---on update cascade;
+alter table tb_pizza ADD CONSTRAINT FK_tbPizza_tbProducto
+foreign key (id_producto) references tb_producto(id_producto)
+on update cascade;
+
+alter table tb_pizzaDetalle ADD CONSTRAINT FK_tbPizzaDetalle_tbPizza
+foreign key (id_pizza) references tb_pizza(id_pizza)
+on update cascade;
+
+alter table tb_pizzaDetalle ADD CONSTRAINT FK_tbPizzaDetalle_tbIngrediente
+foreign key (id_ingrediente) references tb_ingredientes(id_ingrediente)
+on update cascade;
+
+alter table tb_producto ADD CONSTRAINT FK_tbProducto_tbTipo
+foreign key (id_tipo) references tb_tipo(id_tipo)
+on update cascade;
 
 
+alter table tb_refresco ADD CONSTRAINT FK_tbRefresco_tbProducto
+foreign key (id_producto) references tb_producto(id_producto)
+on update cascade;
+
+alter table tb_pedido_info ADD CONSTRAINT FK_tbPedidoInfo_tbEstado
+foreign key (id_estado) references tb_estado(id_estado)
+on update cascade;
+
+alter table tb_pedido ADD CONSTRAINT FK_tbPedido_tbPedidoInfo
+foreign key (id_pedido_info) references tb_pedido_info(id_pedido_info)
+on update cascade;
+
+alter table tb_pedido ADD CONSTRAINT FK_tbPedido_tbProducto
+foreign key (id_producto) references tb_producto(id_producto)
+on update cascade;
+
+alter table tb_pedido ADD CONSTRAINT FK_tbPedido_tbCliente
+foreign key (id_cliente) references tb_cliente(id_cliente)
+on update cascade;
+
+alter table tb_detalleFactura ADD CONSTRAINT FK_tbDetalleFactura_tbPedido
+foreign key (id_pedido) references tb_pedido(id_pedido)
+on update cascade;
+
+alter table tb_detalleFactura ADD CONSTRAINT FK_tbDetalleFactura_tbFactura
+foreign key (id_factura) references tb_Factura(id_factura)
+on update cascade;
+
+alter table tb_cliente ADD CONSTRAINT FK_tbCliente_tbUsuario
+foreign key (id_usuario) references Usuario(id_usuario)
+on update cascade;
+
+alter table tb_empleado ADD CONSTRAINT FK_tbEmpleado_tbUsuario
+foreign key (id_usuario) references Usuario(id_usuario)
+on update cascade;
+
+alter table tb_estado ADD CONSTRAINT FK_tbEstado_tbEmpleado
+foreign key (id_empleado) references tb_empleado(id_empleado)
+on update cascade;
+
+
+INSERT INTO tb_tipo(`nombre`) VALUES 
+('Pizza'),
+('Refrescos');
+
+INSERT INTO tb_producto(`nombre`, `precio`, `imagen`, id_tipo ) VALUES 
+('Pizza Barbacoa', 14.95, 'Pizz', 1),
+('Pizza Carbonara', 13.55, 'Pizz', 1),
+('Coca-Cola', 1.8, 'beb', 2),
+('Aquarius', 1.6, 'beb', 2);
+
+INSERT INTO Usuario(`dni`, `nombre`, `apellidos`, `contraseña`, `imagen`, `tipo_usuario`, `correo` ) VALUES 
+('46472595Z', 'Javi', 'Delgado', 'admin','imagen', 'admin', 'javii.delgaado@gmail.com');
