c04ff5147a75245002e778c9823746be13efe732
david <david.ramirez.sds@gmail.com>
2018-05-11 10:31:53 +0200

    Modelo modificado sin dao sin db connect

--- Modelo/Modelo/Cliente.cs
+++ Modelo/Modelo/Cliente.cs
@@ -9,7 +9,6 @@ namespace Modelo
     public class Cliente : Usuario
     {
 
-
         public Cliente(long id_usuario, string dni, string nombre, string apellidos, string password, string imagen, string tipo_Usuario, string correo) : base(id_usuario, dni, nombre, apellidos, password, imagen, tipo_Usuario, correo)
         {
             
